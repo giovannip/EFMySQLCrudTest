c74c2bfc42645c6f2e0898f3531c5bbbbadc679b
giovannip <prudencio.giovanni@gmail.com>
2014-07-10 01:07:27 -0300

    Create repository

---
+++ EFMySQLCrudTest1/Controllers/EmployeesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFMySQLCrudTest1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private northwindEntities db = new northwindEntities();
+
+        //
+        // GET: /Employees/
+
+        public ActionResult Index()
+        {
+            return View(db.employees.ToList());
+        }
+
+        //
+        // GET: /Employees/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Employees/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employees/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        //
+        // GET: /Employees/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Employees/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Employees/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Employees/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            employee employee = db.employees.Find(id);
+            db.employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EFMySQLCrudTest1/northwind.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\_Projects\mvc\mvc4\EFMySQLCrudTest1\EFMySQLCrudTest1\northwind.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
